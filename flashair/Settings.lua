797ada01196052df0579723f8fb4461b7cb06069
Martin Schalter <schalter.dev@gmail.com>
2019-09-17 10:14:19 +0200

    small improvements

--- flashair/Settings.lua
+++ flashair/Settings.lua
@@ -1,4 +1,4 @@
--- FTP Settings --
+-- Settings --
 logfile   = "/Log.txt"     -- Where to log output on the FA
 folderUpload = "/DCIM"           -- What folder to upload files from
 server    = "	192.168.178.20"  -- The server's IP
